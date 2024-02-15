11ba537a61653b8408e625cd337a63b41e88bf32
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2024-02-15 18:47:17 +0900

    [feat] add express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("[init] this is node typescript project!!");
+import express from "express";
+
+const app = express();
+const port = 5555;
+
+app.get("/health-check", (_, res) => {
+  res.json("Server state is Good");
+});
+
+app.listen(port, () => {
+  console.log(`Server id Running at Port No"${port}"`);
+});
